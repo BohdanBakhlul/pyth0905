881bed8007a8221a5ff617239b62ee1085392739
BohdanBakhlul <pextoshka@gmail.com>
2019-05-16 09:31:58 +0200

    pyth0905

--- 5.py
+++ 5.py
@@ -2,5 +2,11 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 data = np.loadtxt('populacje.txt')
+a = np.mean(data[0])
+b = np.mean(data[1])
+c = np.mean(data[2])
+print(a)
+print(b)
+print(c)
 plt.plot(data)
 plt.show()
